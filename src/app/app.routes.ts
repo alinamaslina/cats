fafc3921f829eb30ff3e4404a59cfea4334d6b05
alinamaslina <alinabezuhla040492@gmail.com>
2025-01-07 22:51:02 +0400

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { CatCardComponent } from './cat-card/cat-card.component';
+import { CatsListComponent } from './cats-list/cats-list.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+//http://localhost:4200/ --> AppComponent
+//http://localhost:4200/cat-profile --> CatCardComponent
+//http://localhost:4200/cat-list --> CatCardComponent
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'cat-profile',
+    component: CatCardComponent,
+  },
+  {
+    path: 'cat-list',
+    component: CatsListComponent,
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
